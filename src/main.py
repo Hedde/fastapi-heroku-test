60acd3efb95cde675a14c0a683fba539e7dd6539
Hedde van der Heide <me@heddevanderheide.nl>
2019-12-18 22:30:04 +0100

    initial commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import socket
+import sys
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+hostname = socket.gethostname()
+
+version = f"{sys.version_info.major}.{sys.version_info.minor}"
+
+
+@app.get("/")
+async def read_root():
+    return {
+        "name": "my-app",
+        "host": hostname,
+        "version": f"Hello world! From FastAPI running on Uvicorn. Using Python {version}"
+    }
